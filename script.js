231c50b8b28760c93da56f8bdc0b08c359bc4e60
HION-KIM <ehdgus7061@gmail.com>
2025-07-31 18:14:05 +0900

    250731 카페 수정 저장

--- script.js
+++ script.js
@@ -1,8 +1,108 @@
-var scrollTop=$(window).scrollTop();
+// ------- header js start -------
+function SearchBtnActive() {
+    $(".search-btn").click(function () {
+        $(".search-input").addClass('active').focus();
+    });
+}
+
+function TopbarHide() {
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop >= 400) {
+            $('.topbar__inner-1').addClass('hide');
+            $('.topbar__inner-2').addClass('active');
+            $()
+        }
+        else if (scrollTop == 0) {
+            $('.topbar__inner-1').removeClass('hide');
+            $('.topbar__inner-2').removeClass('active');
+        }
+    })
+}
+
+function hambergerActive() {
+    $('.topbar__inner-2__hamberger').click(function () {
+        $('.hambergerbox').addClass('active');
+
+    });
+
+    $('.HBG-close-btn').click(function () {
+        $('.hambergerbox').addClass('closed');
+
+        setTimeout(function () {
+            $('.hambergerbox').removeClass('active');
+            $('.hambergerbox').removeClass('closed');
+        }, 600);
+    });
+}
 
 
-$(window).scroll(function(){
-    var scrollTop=$(window).scrollTop();
-    
+
+SearchBtnActive();
+TopbarHide();
+hambergerActive();
+
+// ------- header js end -------
+
+
+
+// ------- banner js start -------
+var swiper = new Swiper(".mySwiper", {
+    effect: "fade",
+    fadeEffect: {
+        crossFade: false,
+    },
+    speed: 1300,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".next",
+        prevEl: ".prev",
+    },
+
+    touchRatio: 0.5,
+});
+// ------- banner js end -------
+
+
+
+// ------- main js start -------
+AOS.init();
+
+
+function productMenuActive() {
+    $(window).scroll(function () {
+        var scrollTop2 = $(window).scrollTop();
+
+        if (scrollTop2 >= 1000) {
+            $('.product-menu').addClass('active');
+        } else {
+            $('.product-menu').removeClass('active');
+        }
+    });
+}
+
+productMenuActive();
+// ------- main js end -------
+
+
+// ------- footer js start -------
+// ------- footer js end -------
+
+
+var scrollTop = $(window).scrollTop();
+
+
+$(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+
     console.log(scrollTop);
 });
